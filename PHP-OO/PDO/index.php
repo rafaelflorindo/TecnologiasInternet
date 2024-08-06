8bee241ff55e8987363718849f7a7b257b9124eb
Rafael Alves Florindo <rafaelflorindo@hotmail.com>
2024-08-05 21:04:05 -0300

    Add files via upload

--- PHP-OO/PDO/index.php
+++ PHP-OO/PDO/index.php
@@ -4,7 +4,7 @@
     Base de dados TintasDark
     tabela cliente
     campos: id, nome, cpf, cnpj, endereco, numero, bairro, cep, cidadel, email, 
-    data de cadatro e data de atualização
+    data de cadastro e data de atualização
 */
 
 $username ="root";
@@ -12,4 +12,34 @@ $password = "";
 
 $conn = new PDO('mysql:host=localhost;dbname=TintasDark', $username, $password);
 
-var_dump($conn);
+/*
+$selectFull = $conn->query("SELECT * FROM pessoa");
+foreach($selectFull as $row) {
+    echo "<pre>";
+    print_r($row);
+    echo "</pre>";
+}
+*/    
+
+//preparação de consulta
+$selectFull = $conn->prepare('SELECT * FROM pessoa');
+$selectFull->execute();
+
+/*
+$id = 1;
+$selectFull = $conn->prepare('SELECT * FROM pessoa where id= :id');
+$selectFull->execute(array('id' => $id));
+$selectFull->execute();
+*/
+
+$rows = array();
+while($row = $selectFull->fetchALL(PDO::FETCH_ASSOC)){
+//while($row = $selectFull->fetchALL(PDO::FETCH_OBJ)){
+   $rows[] = $row;
+}
+
+foreach($rows as $row) {
+    echo "<pre>";
+    print_r($row);
+    echo "</pre>";
+}
